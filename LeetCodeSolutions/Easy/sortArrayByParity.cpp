cf6aa68aae7e3c56d3e6dfbd410eb55e70976e8d
Dathan Lang <dathan_lang@outlook.com>
2025-01-14 14:55:53 -0500

    alternate code for sortArrayByParity

--- LeetCodeSolutions/Easy/sortArrayByParity.cpp
+++ LeetCodeSolutions/Easy/sortArrayByParity.cpp
@@ -14,3 +14,19 @@ public:
         return even;
     }
 };
+
+class Solution2 {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> answer;
+        for(int i = 0; i < nums.size();i++)
+        {
+            if(nums[i]%2==0) answer.push_back(nums[i]);
+        }
+        for(int i = 0; i < nums.size();i++)
+        {
+            if(nums[i]%2!=0) answer.push_back(nums[i]);
+        }
+        return answer;
+    }
+};
